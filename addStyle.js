da87e0b5d9dbf4fb4a1b569ea07834f3f4f02ae8
petehunt <floydophone@gmail.com>
2014-02-28 18:16:22 -0800

    Make isomorphic

--- addStyle.js
+++ addStyle.js
@@ -2,7 +2,14 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+var isInBrowser = require('./isInBrowser');
+var emptyFunction = require('./emptyFunction');
+
 module.exports = function(cssCode) {
+	if (!isInBrowser()) {
+		return emptyFunction;
+	}
+
 	var styleElement = document.createElement("style");
 	styleElement.type = "text/css";
 	if (styleElement.styleSheet) {
