ac02038addbf441fc793fcd929c1dd8cc2501ec6
Anson Yu Wang <opensource@undozen.com>
2014-08-12 18:31:46 +0800

    enable hot replace in url.js

--- url.js
+++ url.js
@@ -7,8 +7,10 @@ module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
 	this.cacheable && this.cacheable();
 	var comment1 = "// style-loader: Adds some css to the DOM by adding a <style> tag\n";
-	var addStyleCode = "require(" + JSON.stringify("!" + path.join(__dirname, "addStyleUrl.js")) + ")\n";
+	var addStyleCode = "var dispose = require(" + JSON.stringify("!" + path.join(__dirname, "addStyleUrl.js")) + ")\n";
 	var comment2 = "\t// The url to the css code:\n";
 	var cssUrlRequest = "require(" + JSON.stringify("!!" + remainingRequest) + ")";
-	return comment1 + addStyleCode + comment2 + "\t(" + cssUrlRequest + ")";
+	var comment3 = "// Hot Module Replacement\n";
+	var hmrCode = "if(module.hot) {\n\tmodule.hot.accept();\n\tmodule.hot.dispose(dispose);\n}";
+	return comment1 + addStyleCode + comment2 + "\t(" + cssUrlRequest + ");\n" + comment3 + hmrCode;
 };
