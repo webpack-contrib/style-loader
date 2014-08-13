2790600304a18dfae6055c9f696d41f154e1ff26
Simon Degraeve <simon.degraeve@gmail.com>
2014-08-13 16:54:14 +0800

    Clean and fix "not reloading after error" bug

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+
 var path = require("path");
+
 module.exports = function() {};
+
 module.exports.pitch = function(remainingRequest) {
 	this.cacheable && this.cacheable();
-	var comment1 = "// style-loader: Adds some css to the DOM by adding a <style> tag\n";
-	var addStyleCode = "var dispose = require(" + JSON.stringify("!" + path.join(__dirname, "addStyle.js")) + ")\n";
-	var comment2 = "\t// The css code:\n";
-	var cssCodeRequest = "require(" + JSON.stringify("!!" + remainingRequest) + ")";
-	var comment3 = "// Hot Module Replacement\n";
-	var hmrCode = "if(module.hot) {\n\tmodule.hot.accept();\n\tmodule.hot.dispose(dispose);\n}";
-	return comment1 + addStyleCode + comment2 + "\t(" + cssCodeRequest + ");\n" + comment3 + hmrCode;
+	
+	return [
+		'// style-loader: Adds some css to the DOM by adding a <style> tag',
+		'',
+		'var css = require(' + JSON.stringify('!!' + remainingRequest) + ');',
+		'var dispose = require(' + JSON.stringify('!' + path.join(__dirname, 'addStyle.js')) + ')(css);',
+		'',
+		'if (module.hot) {',
+		'  module.hot.accept(' + JSON.stringify('!!' + remainingRequest) + ');',
+		'  module.hot.dispose(dispose);',
+		'}',
+	].join('\n');
 };
