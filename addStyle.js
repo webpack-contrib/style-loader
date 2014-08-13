3d38054a017965e22771ffcaafb1df1bf86c788f
Anson Yu Wang <opensource@undozen.com>
2014-08-13 17:41:38 +0800

    keep style tag position

--- addStyle.js
+++ addStyle.js
@@ -2,20 +2,21 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-module.exports = function addStyle(cssCode) {
+module.exports = function addStyle(cssCode, nextElementSibling) {
 	if(typeof DEBUG !== "undefined" && DEBUG) {
 		if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
 	}
 	var styleElement = document.createElement("style");
 	styleElement.type = "text/css";
 	var head = document.getElementsByTagName("head")[0];
-	head.appendChild(styleElement);
+	head.insertBefore(styleElement, nextElementSibling || null);
 	if (styleElement.styleSheet) {
 		styleElement.styleSheet.cssText = cssCode;
 	} else {
 		styleElement.appendChild(document.createTextNode(cssCode));
 	}
-	return function() {
+	return function(data) {
+		data.nextEl = styleElement.nextElementSibling;
 		head.removeChild(styleElement);
 	};
 }
