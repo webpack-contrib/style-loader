941a6f05bc882378e0ff21c57087ba50c0e4ab06
Anson Yu Wang <opensource@undozen.com>
2014-09-12 14:01:51 +0800

    use link element which has same href (ignoring querystring and hash)

--- addStyleUrl.js
+++ addStyleUrl.js
@@ -6,12 +6,15 @@ module.exports = function addStyleUrl(cssUrl) {
 	if(typeof DEBUG !== "undefined" && DEBUG) {
 		if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
 	}
-	var styleElement = document.createElement("link");
-	styleElement.rel = "stylesheet";
-	styleElement.type = "text/css";
-	styleElement.href = cssUrl;
-	var head = document.getElementsByTagName("head")[0];
-	head.appendChild(styleElement);
+	var styleElement = document.querySelector('head link[href^="'+cssUrl.replace(/[\?#].*$/, '')+'"]');
+	if (!styleElement) {
+		styleElement = document.createElement("link");
+		styleElement.rel = "stylesheet";
+		styleElement.type = "text/css";
+		styleElement.href = cssUrl;
+		var head = document.getElementsByTagName("head")[0];
+		head.appendChild(styleElement);
+	}
 	if(module.hot) {
 		return function(cssUrl) {
 			if(typeof cssUrl === "string") {
