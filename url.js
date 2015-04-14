7526b57ad205ca9b9b83e6fa1196d5e6e164455b
Evgeny Reznichenko <kusakyky@gmail.com>
2015-04-14 15:47:10 +0300

    Use loaderUtils#stringifyRequest

--- url.js
+++ url.js
@@ -2,19 +2,20 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-var path = require("path");
+var loaderUtils = require("loader-utils"),
+	path = require("path");
 module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
 	this.cacheable && this.cacheable();
 	return [
 		"// style-loader: Adds some reference to a css file to the DOM by adding a <link> tag",
 		"var update = require(" + JSON.stringify("!" + path.join(__dirname, "addStyleUrl.js")) + ")(",
-		"\trequire(" + JSON.stringify("!!" + remainingRequest) + ")",
+		"\trequire(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + ")",
 		");",
 		"// Hot Module Replacement",
 		"if(module.hot) {",
-		"\tmodule.hot.accept(" + JSON.stringify("!!" + remainingRequest) + ", function() {",
-		"\t\tupdate(require(" + JSON.stringify("!!" + remainingRequest) + "));",
+		"\tmodule.hot.accept(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + ", function() {",
+		"\t\tupdate(require(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + "));",
 		"\t});",
 		"\tmodule.hot.dispose(function() { update(); });",
 		"}"
