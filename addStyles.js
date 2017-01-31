e908a1f5e26126ac720a181d64570e52e1947d91
Ilan Copelyn <ilan@netreturn.co.za>
2017-01-31 22:38:31 +0200

    cast options.cssBase to number

--- addStyles.js
+++ addStyles.js
@@ -105,7 +105,7 @@ function listToStyles(list, options) {
 	var newStyles = {};
 	for(var i = 0; i < list.length; i++) {
 		var item = list[i];
-		var id = item[0] + (options.cssBase || 0);
+		var id = item[0] + (+(options.cssBase || 0));
 		var css = item[1];
 		var media = item[2];
 		var sourceMap = item[3];
