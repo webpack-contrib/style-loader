6c3b2f70e67fbeedde490207e37e0110366e413d
danielverejan <danie.verejan@gmail.com>
2017-02-22 14:39:45 +0200

    Add replace option: search for a css reference and replace it's href

---
+++ replace.js
@@ -0,0 +1,19 @@
+var loaderUtils = require("loader-utils"),
+	path = require("path");
+module.exports = function() {};
+module.exports.pitch = function(remainingRequest) {
+	this.cacheable && this.cacheable();
+	return [
+		"// style-loader: Search for a css file reference in the DOM and replace the <link> href",
+		"var update = require(" + JSON.stringify("!" + path.join(__dirname, "replaceStyleUrl.js")) + ")(",
+		"\trequire(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + ")",
+		");",
+		"// Hot Module Replacement",
+		"if(module.hot) {",
+		"\tmodule.hot.accept(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + ", function() {",
+		"\t\tupdate(require(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + "));",
+		"\t});",
+		"\tmodule.hot.dispose(function() { update(); });",
+		"}"
+	].join("\n");
+};
---
+++ replaceStyleUrl.js
@@ -0,0 +1,41 @@
+function getFileName(str) {return (str.match(/[^\\/]+\.[^\\/]+$/) || []).pop()}
+
+function replaceUrl(cssUrl, styleSheets) {
+	for (var i = 0; i < styleSheets.length; i++) {
+		var href = styleSheets[i].href.split("?")[0];
+		var url = cssUrl.split("?")[0];
+
+		var hrefFileName = getFileName(href);
+		var urlFileName = getFileName(url);
+
+		if (hrefFileName === urlFileName) {
+			styleSheets[i].href = cssUrl;
+			break;
+		}
+	}
+}
+
+module.exports = function replaceStyleUrl(cssUrl) {
+	if(typeof DEBUG !== "undefined" && DEBUG) {
+		if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
+	}
+
+	var links = document.getElementsByTagName("link");
+	var styleSheets = [];
+
+	for (var i = 0; i < links.length; i++) {
+		if (links[i].href && links[i].rel === "stylesheet") styleSheets.push(links[i]);
+	}
+
+	if (!styleSheets.length) return;
+
+	replaceUrl(cssUrl, styleSheets);
+
+	if(module.hot) {
+		return function(cssUrl) {
+			if(typeof cssUrl === "string") {
+				replaceUrl(cssUrl, styleSheets);
+			}
+		};
+	}
+}
