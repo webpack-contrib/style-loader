c57b43f57e70bfcc32655595c65780140844401d
danielverejan <danie.verejan@gmail.com>
2017-02-24 10:18:22 +0200

    Fix Codacy code issues

--- replaceStyleUrl.js
+++ replaceStyleUrl.js
@@ -1,37 +1,45 @@
+/* global DEBUG */
 /*
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-function getFileName(str) {return (str.match(/[^\\/]+\.[^\\/]+$/) || []).pop()}
+function getFileName(path) {
+	var urlToFile = path.split("?")[0];
+	return (urlToFile.match(/[^\\/]+\.[^\\/]+$/) || []).pop();
+}
 
 function replaceUrl(cssUrl, styleSheets) {
 	for (var i = 0; i < styleSheets.length; i++) {
-		var href = styleSheets[i].href.split("?")[0];
-		var url = cssUrl.split("?")[0];
-
-		var hrefFileName = getFileName(href);
-		var urlFileName = getFileName(url);
+		var hrefFileName = getFileName(styleSheets[i].href);
+		var urlFileName = getFileName(cssUrl);
 
 		if (hrefFileName === urlFileName) {
 			styleSheets[i].href = cssUrl;
-			break;
 		}
 	}
 }
 
-module.exports = function replaceStyleUrl(cssUrl) {
-	if(typeof DEBUG !== "undefined" && DEBUG) {
-		if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
-	}
-
+function getStyleSheets() {
 	var links = document.getElementsByTagName("link");
 	var styleSheets = [];
 
 	for (var i = 0; i < links.length; i++) {
-		if (links[i].href && links[i].rel === "stylesheet") styleSheets.push(links[i]);
+		if (links[i].href && links[i].rel === "stylesheet") {
+			styleSheets.push(links[i]);
+		}
 	}
 
-	if (!styleSheets.length) return;
+	return styleSheets;
+}
+
+module.exports = function replaceStyleUrl(cssUrl) {
+	if(typeof DEBUG !== "undefined" && DEBUG) {
+		if(typeof document !== "object") {
+			throw new Error("The style-loader cannot be used in a non-browser environment");
+		}
+	}
+
+	var styleSheets = getStyleSheets();
 
 	replaceUrl(cssUrl, styleSheets);
 
@@ -42,4 +50,4 @@ module.exports = function replaceStyleUrl(cssUrl) {
 			}
 		};
 	}
-}
+};
