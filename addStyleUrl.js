2c4f4278b0d038ae1d022ff17b5c2e3f86d64fe3
Konstantin Kai <kosyak46@gmail.com>
2017-03-22 15:43:24 +0200

    feat: Set custom attributes for tag in url mode (#198)
    * Add ability to set custom attributes for tag in url mode

--- addStyleUrl.js
+++ addStyleUrl.js
@@ -2,14 +2,28 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-module.exports = function addStyleUrl(cssUrl) {
+
+function attachTagAttrs(element, attrs) {
+	Object.keys(attrs).forEach(function (key) {
+		element.setAttribute(key, attrs[key]);
+	});
+}
+
+module.exports = function addStyleUrl(cssUrl, options) {
 	if(typeof DEBUG !== "undefined" && DEBUG) {
 		if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
 	}
+
+	options = options || {};
+	options.attrs = typeof options.attrs === "object" ? options.attrs : {};
+
 	var styleElement = document.createElement("link");
 	styleElement.rel = "stylesheet";
 	styleElement.type = "text/css";
 	styleElement.href = cssUrl;
+
+	attachTagAttrs(styleElement, options.attrs);
+
 	var head = document.getElementsByTagName("head")[0];
 	head.appendChild(styleElement);
 	if(module.hot) {
