b516fc01d64b9a291512b1605d52c6af1f7dbf29
Simon Lydell <simon.lydell@gmail.com>
2018-01-26 19:42:09 +0100

    Add missing semicolon

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ module.exports.pitch = function (request) {
 		"			return idx === 0;",
 		"		}(content.locals, newContent.locals));",
 		"		// This error is caught and not shown and causes a full reload.",
-		"		if(!locals) throw new Error('Aborting CSS HMR due to changed css-modules locals.')",
+		"		if(!locals) throw new Error('Aborting CSS HMR due to changed css-modules locals.');",
 		"		update(newContent);",
 		"	});",
 		"	// When the module is disposed, remove the <style> tags",
