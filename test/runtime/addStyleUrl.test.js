ad5d6a783f03be3a688d2f7fefbca3dcfd1bdac8
evilebottnawi <sheo13666q@gmail.com>
2019-07-27 18:30:38 +0300

    test: unit

---
+++ test/runtime/addStyleUrl.test.js
@@ -0,0 +1,18 @@
+/* eslint-env browser */
+
+import addStyleUrl from '../../src/runtime/addStyleUrl';
+
+describe('addStyle', () => {
+  beforeEach(() => {
+    document.head.innerHTML = '<title>Title</title>';
+    document.body.innerHTML = '<h1>Hello world</h1>';
+  });
+
+  // Each query should have be unique because style-loader caching styles in dom
+
+  it('should work', () => {
+    addStyleUrl('./style-1.css');
+
+    expect(document.documentElement.innerHTML).toMatchSnapshot();
+  });
+});
