08a8cd3bfa9d0f8f10edc8662283ccaa3cf7976d
cap-Bernardito <cap-Bernardito@yandex.ru>
2021-05-22 20:08:30 +0300

    test: refactor

--- test/fixtures/es-modules.js
+++ test/fixtures/es-modules.js
@@ -1,15 +1,3 @@
 import * as mod from './es-modules.css';
 
-const node1 = document.createElement("DIV");
-
-const textNode1 = document.createTextNode(`
-============================
-EXPORTS:
-============================
-${JSON.stringify(mod, null, '\t')}
-============================
-`);
-
-node1.appendChild(textNode1);
-
-document.body.appendChild(node1);
+window.__cssLoader = mod;
